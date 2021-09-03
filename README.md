372cb8a800b65feba2be8ea15c5243ae2a008aba
msio900 <msio900@gmail.com>
2021-09-03 16:09:21 +0900

    0903 ms 1st commit

--- README.md
+++ README.md
@@ -1,25 +1,28 @@
-# 텍스트 마이닝을 이용한 트렌드 예측 서비스📋
+# 📰텍스트 마이닝을 이용한 트렌드 예측 서비스
 
 > 멀티캠퍼스의 '빅데이터 기반 지능형 서비스 개발' 프로그램의 `HEXinAR` 팀의 `EXA_wave` 서비스 프로젝트 입니다.
 
-#### Contents
 
-1. [overview](#overview)
-2. [skills](#skills)
-3. [team](#team)
+
+#### 📑Contents
+
+1. [Overview](#Overview)
+2. [Subject](#Subject)
 4. [project scheduling](#project-scheduling)
 
 
 
-## overview
+------
 
+## Overview
+
+* Subject :
+* Team
 * 
-  
-* 
 
 
 
-## skills
+## Subject
 
 *  
 
@@ -27,14 +30,7 @@
 
 * 
 
-## team
 
-* 
-
-
-
-* 
-   
 
 ## Project Scheduling
 
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os, json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fp+7!z)*ay4gevn&dk)&7fqqz$bjw%rzct8tyo0^fnf3p@cqwq'
+from django.core.exceptions import ImproperlyConfigured
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
